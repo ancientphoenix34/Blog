d16f897ac40249d48368407f2f30324bd5e928d2
ancientphoenix34 <farsukoothradan@gmail.com>
2024-09-03 16:04:16 +0530

    server side complte

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -96,21 +96,107 @@ const {id}=req.params
 const posts=await Post.find({creator:id}).sort({createdAt:-1})
 res.status(200).json(posts)
     }
-    catch{
+    catch(error){
         return next(new HttpError(error))
     }
 }
 
 
 const editPost=async(req,res,next)=>{
-    res.json("Edit")
+    try{
+let fileName;
+let newFilename;
+let updatedPost;
+const postId=req.params.id;
+
+let {title,category,description}=req.body;
+
+
+//to be compatible with react quill,11 characters will already be there
+if(!title || !category || description<12 ){
+    return next(new HttpError("Please enter all fields",422))
+}
+
+//change post without altering thumbnail
+const oldPost=await Post.findById(postId);
+if(req.user.id==oldPost.creator){
+if(!req.files){
+    updatedPost=await Post.findByIdAndUpdate(postId,{title,category,description},{new:true})
+}
+//thumbnail changing
+else{
+//get old post
+const oldPost=await Post.findById(postId);
+
+//delete old thumbnail
+ fs.unlink(path.join(__dirname,'..','uploads',oldPost.thumbnail),async(err)=>{
+    if(err){
+        return next(new HttpError(err))
+    }
+   
+})
+ //upload new thumbnail
+ const {thumbnail}=req.files
+ if(thumbnail.size>5000000){
+     return next(new HttpError("Thumbnail should be less than 5mb",422))
+ }
+ fileName=thumbnail.name;
+ let splittedFilename=fileName.split('.')
+ newFilename=splittedFilename[0]+uuid()+'.'+splittedFilename[splittedFilename.length-1]
+ thumbnail.mv(path.join(__dirname,'..','uploads',newFilename),async(err)=>{
+   if(err){
+     return next(new HttpError(err))
+   }  
+ })
+ updatedPost=await Post.findByIdAndUpdate(postId,{title,category,description,thumbnail:newFilename},{new:true})
+}
+}
+
+if(!updatedPost){
+    return next(new HttpError("Post could'nt be updated",422))
+}
+res.status(200).json(updatedPost)
+
+    }
+    
+    catch(error){
+        return next(new HttpError(error))
+    }
 }
 
 
 
 
 const deletePost=async(req,res,next)=>{
-    res.json("delete post")
+try{
+const postId=req.params.id;
+if(!postId){
+    return next(new HttpError("Post not found",404))
+}
+const post=await Post.findById(postId)
+const fileName=post?.thumbnail;
+//del pic from uploads
+if(req.user.id==post.creator){
+fs.unlink(path.join(__dirname,'..','uploads',fileName),async(err)=>{
+    if(err){
+        return next(new HttpError(err))
+    }else{
+await Post.findByIdAndDelete(postId);
+
+//post count
+
+const currentUser=await User.findById(req.user.id)
+const userPostCount=currentUser?.posts-1
+await User.findByIdAndUpdate(req.user.id,{posts:userPostCount},{new:true})
+res.json(`Post ${postId} deleted successfully`)
+    }
+})
+}else{
+    return next(new HttpError("You are not authorized to delete this post",403))
+}
+}catch(error){
+ return next(new HttpError(error))
+}
 }
 
 
