1a74c6fdd84f333f01db5b10d3cbc9e23ac919eb
Farseen <farsukoothradan@gmail.com>
2026-05-20 10:38:50 +0530

    env variable change done

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app=express();
 
 app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
-app.use(cors({ credentials: true,  origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: process.env.CLIENT_ORIGIN }));
 // file upload
 app.use(upload());
 app.use('/uploads',express.static(__dirname+'/uploads'))
